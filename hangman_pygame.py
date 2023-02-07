78dd1775f8ce2366002b5d41ea36174826a5e59e
codeit1056 <114349851+codeit1056@users.noreply.github.com>
2023-02-07 21:14:03 +0000

    Update hangman_pygame.py

--- hangman_pygame.py
+++ hangman_pygame.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import hangman_pygame_words as words
+import random
 pygame.init()
 
 WIDTH,HEIGHT = 800,600
@@ -13,12 +14,17 @@ pygame.display.set_caption('Hangman')
 clock = pygame.time.Clock()
 
 font = pygame.font.Font('hangman_assets/hangman_font.ttf',15)
+gap_font = pygame.font.Font('hangman_assets/hangman_font.ttf',100)
 
 button_letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 button_radius = 20
 buttons = []
+buttons_and_letters = []
 
-def draw_buttons():
+random_word = random.choice(words.words_list)
+random_word_lst = list(random_word)
+
+def draw_buttons(button_letters):
     for button in range(10):
         pygame.draw.circle(window,BLACK,((button * 65) + 110,350),button_radius,4)
         buttons.append(pygame.draw.circle(window,BLACK,((button * 65) + 110,350),button_radius,4))
@@ -29,13 +35,37 @@ def draw_buttons():
         pygame.draw.circle(window,BLACK,((button * 65) + 110,450),button_radius,4)
         buttons.append(pygame.draw.circle(window,BLACK,((button * 65) + 110,450),button_radius,4))
 
-    for letter in button_letters:
+
+    for letter, button in zip(button_letters, buttons):
+        buttons_and_letters.append([button,letter])
         letter_ = font.render(letter,True,BLACK)
-        for button in buttons:
-            letter_rect = letter_.get_rect(center = (button.centerx,button.centery))
-            window.blit(letter_,letter_rect)
+        letter_rect = letter_.get_rect(center = (button.centerx,button.centery))
+        window.blit(letter_,letter_rect)
 
+def make_gaps():
+    gaps = []
+    for letter in random_word_lst:
+        gaps.append('_')
+    for gap in gaps:
+        for i in range(len(random_word_lst)):
+            gap_place = pygame.image.load('hangman_assets/gap.xcf').convert_alpha()
+            gap_rect = gap_place.get_rect(center = ((i*60) + 250,275))
+            window.blit(gap_place,gap_rect)
 
+def over_button():
+    mouse_pos = pygame.mouse.get_pos()
+    over_button = False
+    for button in buttons:
+        if button.collidepoint(mouse_pos):
+            over_button = True
+
+    return over_button
+
+def return_letter_pressed():
+    mouse_pos = pygame.mouse.get_pos()
+    for button_pair in buttons_and_letters:
+        if button_pair[0].collidepoint(mouse_pos):
+            return button_pair[0],button_pair[1],button_pair
 
 def run():
     while True:
@@ -43,9 +73,16 @@ def run():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN and over_button():
+                button,letter,button_pair = return_letter_pressed()
+                button_letters.remove(letter)
+                buttons.remove(button)
         
         window.fill(WHITE)
-        draw_buttons()
+        draw_buttons(button_letters)
+        make_gaps()
+        over_button()
         pygame.display.update()
         clock.tick(FPS)
 
